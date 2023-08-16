3d3e7f47216e41a652141665a7816e39c49aad82
vini2sousa <135989617+vini2sousa@users.noreply.github.com>
2023-08-16 13:52:04 -0300

    projeto finalizado

---
+++ src/java/java.js
@@ -0,0 +1,13 @@
+function logar(){
+
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == "fontejr@gmail.com" && senha == "fontejr123"){
+        window.location.href = "dashboard.html";
+      }
+      else{
+        alert('Usuario ou senha incorretos');
+    }
+
+}
