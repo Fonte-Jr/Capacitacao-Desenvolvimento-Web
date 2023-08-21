80e241fd8c06b36f32e775aa0c4b298b92bbbaa1
euqirn3h <henrique11rodrigues@gmail.com>
2023-08-21 19:10:57 -0300

    Adding script updates

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,9 @@
 function Login(){
-    let email = document.getElementsByName('email').value;
-    let password = document.getElementsByName('password').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
 
-    if(email === "email@email.com" && password === "1234") {
-        window.location.href = "dashboard.html";
+    if(email == "email@email.com" && password == "1234") {
+        window.location.href = "dashboard.html"
     }
     else{
         console.log(email,password);
