9eba883f9c0ee51ca0ebb874801764ac5c5b6093
euqirn3h <henrique11rodrigues@gmail.com>
2023-12-07 18:07:14 -0300

    Adicionando CRUD - requisiscoes HTTP

--- backend/api/client/index.js
+++ backend/api/client/index.js
@@ -1,8 +1,64 @@
-let cep = 36080001
-let endpoint = `https://viacep.com.br/ws/${cep}/json/`;
-let response =  fetch(endpoint, {
-    method: "GET"
+let endpoint = `http://localhost:3000/User`;
+
+let user = {
+    username: 'user',
+    password: '1234'
+}
+
+fetch(endpoint, {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {"Content-type": "application/json; charset=UTF-8"}
 })
 .then(async (res) => {
     console.log(await res.json());
+
+    fetch(endpoint, {
+        method: "GET"
+    })
+    .then(async (res) => {
+        console.log(await res.json());
+
+        user = {
+            username: 'user',
+            password: '1234555'
+        }
+    
+        fetch(endpoint, {
+            method: "PUT",
+            body: JSON.stringify(user),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        }).then(async (res) => {
+            fetch(endpoint, {
+                method: "GET"
+            })
+            .then(async (res) => {
+                console.log(await res.json());
+
+                user = {
+                    username: 'user'
+                }
+
+                fetch(endpoint, {
+                    method: "DELETE",
+                    body: JSON.stringify(user),
+                    headers: {"Content-type": "application/json; charset=UTF-8"}
+                }).then(async (res) => {
+                    fetch(endpoint, {
+                        method: "GET"
+                    })
+                    .then(async (res) => {
+                        console.log(await res.json());
+                    });
+                });
+            });
+        });
+    });
+
 });
+
+
+
+
+
+
