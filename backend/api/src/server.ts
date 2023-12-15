be00667db5c29d1f4e9b4349022e11a5af3d40cd
euqirn3h <henrique11rodrigues@gmail.com>
2023-12-15 20:40:59 -0300

    Adicionando criptografia para senhas

--- backend/api/src/server.ts
+++ backend/api/src/server.ts
@@ -1,22 +1,26 @@
 import express from 'express';
+import { Role } from './models/Role';
 import { User } from './models/User';
+import { Encrypt } from './utils/Encrypt';
 
 
 const app = express();
 const PORT = 3000;
 const USERS : Array<User> = [];
+const ROLES : Array<Role> = [];
 
 app.use(express.json());
 
-app.post('/User', (req, res) => {
-    let { username, password } = req.body;
+app.post('/User', async (req, res) => {
+    let { username, password, roleId } = req.body;
 
-    console.log(req.body);
-    if(!username || !password) return res.status(400).send({ error: 'Username and Password needed!'});
+    if(!username || !password || !roleId) return res.status(400).send({ error: 'Fields needed!'});
     
     let user: User = {
+        id: USERS.length > 0 ? USERS[USERS.length -1].id + 1 : 1,
         username: username,
-        password: password
+        password: await new Encrypt().getHash(password),
+        roleId: roleId
     };
 
     if(USERS.find( item => item.username === user.username)){
@@ -28,14 +32,16 @@ app.post('/User', (req, res) => {
 });
 
 app.put('/User', (req, res) => {
-    let { username, password } = req.body;
+    let { username, password, roleId } = req.body;
 
     let user: User | undefined = USERS.find( item => item.username === username);
 
     if(user){
         let newUser: User = {
+            id: user.id,
             username: username,
-            password: password
+            password: password,
+            roleId: roleId
         };
 
         let index = USERS.findIndex( item => item.username === newUser.username);
@@ -62,6 +68,57 @@ app.delete('/User', (req, res) => {
 
 app.get('/User', (req, res) => {
     return res.status(200).send(USERS);
+});
+
+app.post('/Role', (req, res) => {
+    let { role } = req.body;
+
+    if(!role) return res.status(400).send({ error: 'Fields needed!'});
+    
+    let newRole: Role = {
+        id: ROLES.length > 0 ? ROLES[ROLES.length -1].id + 1 : 1,
+        role: role
+    };
+
+    ROLES.push(newRole);
+    return res.status(201).send(newRole);
+});
+
+app.put('/Role', (req, res) => {
+    let { role } = req.body;
+
+    let roleDb: Role | undefined = ROLES.find( item => item.role === role);
+
+    if(roleDb){
+        let newRole: Role = {
+            id: roleDb.id,
+            role: role
+        };
+
+        let index = ROLES.findIndex( item => item.role === newRole.role);
+        ROLES[index] = newRole;
+        return res.sendStatus(204);
+    }
+
+    return res.sendStatus(400);
+});
+
+app.delete('/Role', (req, res) => {
+    let { role } = req.body;
+
+    let roleDb: Role | undefined = ROLES.find( item => item.role === role);
+
+    if(roleDb){
+        let index = ROLES.findIndex( item => item.role === role);
+        ROLES.splice(index,1);
+        return res.sendStatus(204);
+    }
+
+    return res.status(400).send('User does not exist!');
+});
+
+app.get('/Role', (req, res) => {
+    return res.status(200).send(ROLES);
 });
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
